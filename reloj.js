946e89b3b8b053b98026a67a6cad85ca3d28f319
Martin Casas <kostenlos98@gmail.com>
2020-11-12 23:05:17 -0300

    Reloj arreglado. Servidor NTP finalizado Cliente con algunas cosas

--- reloj.js
+++ reloj.js
@@ -30,33 +30,30 @@ class Reloj {
     }
 
     actualizarTiempo() {
-        var funcionIntervalo = setInterval(function () {
 
+        var funcionIntervalo = setInterval(function () {
             const client = new net.Socket();
 
             client.connect(this.puerto, this.ip, function () {
-                const T1 = (new Date()).getTime().toString();  /* tiempo de envío del cliente */
-                client.write(T1);
+                const T1 = { "t1": (new Date().toISOString()) }; 
+                client.write(JSON.stringify(T1));
             });
 
             client.on('data', function (respuesta) {
-                const T4 = (new Date()).getTime(); /* tiempo de arribo de respuesta del servidor */
+                const T4 = (new Date()).getTime(); 
                 const tiempos = respuesta.toString().split(',');
 
-                const T1 = parseInt(tiempos[0]);
-                const T2 = parseInt(tiempos[1]);
-                const T3 = parseInt(tiempos[2]);
+                const T1 = Date.parse(tiempos[0]);
+                const T2 = Date.parse(tiempos[1]);
+                const T3 = Date.parse(tiempos[2]);
 
                 const offset = ((T2 - T1) + (T3 - T4)) / 2;
-
                 this.offset = offset;
-
                 client.destroy();
             });
 
-            client.on('close', () => { });
-
         }, this.periodo);
+        funcionIntervalo();
     }
 
 
